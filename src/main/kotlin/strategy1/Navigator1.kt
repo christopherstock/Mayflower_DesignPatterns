787d084d6e81e90ff1bd554b8940f1ab9ec101a0
Christopher Stock <christopher.stock@mayflower.de>
2021-02-27 17:14:14 +0100

    Add Sandbox example 1.

---
+++ src/main/kotlin/strategy1/Navigator1.kt
@@ -0,0 +1,17 @@
+package strategy1
+
+class Route
+class Point
+
+class Navigator1
+{
+    fun buildRoute( A: Point, B: Point ): Route
+    {
+        val route :Route = Route()
+
+        // many lines of code + API invocations
+        // ...
+
+        return route
+    }
+}
