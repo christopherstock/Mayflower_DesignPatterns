3af20852d90d2e58bc87c2e1c7dbb1e8cb5164a2
Christopher Stock <christopher.stock@mayflower.de>
2021-02-28 10:08:44 +0100

    Add Kotlin Linter 'detekt'.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.30"
     application
+    id("io.gitlab.arturbosch.detekt").version("1.15.0")
 }
 
 group = "me.stock"
@@ -10,6 +11,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter {
+        content {
+            // just allow to include kotlinx projects
+            // detekt needs 'kotlinx-html' for the html report
+            includeGroup("org.jetbrains.kotlinx")
+        }
+    }
 }
 
 dependencies {
@@ -28,4 +36,25 @@ tasks.withType<KotlinCompile>() {
 
 application {
     mainClassName = "MainKt"
+}
+
+detekt {
+    // preconfigure defaults
+    buildUponDefaultConfig = true
+    // point to your custom config defining rules to run, overwriting default behavior
+    config = files("$projectDir/config/detekt.yml")
+    // a way of suppressing issues before introducing detekt
+    baseline = file("$projectDir/config/baseline.xml")
+
+    reports {
+        // observe findings in your browser with structure and code snippets
+        html.enabled = true
+        // checkstyle like format mainly for integrations like Jenkins
+        xml.enabled = true
+        // similar to the console output, contains issue signature to manually edit baseline files
+        txt.enabled = true
+        // standardized SARIF format (https://sarifweb.azurewebsites.net/)
+        // to support integrations with Github Code Scanning
+        sarif.enabled = true
+    }
 }
