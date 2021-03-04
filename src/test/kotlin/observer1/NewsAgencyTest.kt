e7d2069d6eff4c8e058ea0652bacb49255e2212d
Christopher Stock <christopher.stock@mayflower.de>
2021-03-04 18:42:19 +0100

    Adjust test data.

--- src/test/kotlin/observer1/NewsAgencyTest.kt
+++ src/test/kotlin/observer1/NewsAgencyTest.kt
@@ -8,8 +8,8 @@ class NewsAgencyTest
     @Test
     fun testObserver() {
         val agency = NewsAgency()
-        agency.updateNews("The latest News")
+        agency.updateNews("Breaking News")
 
-        assertEquals(agency.channelCNN.news, "The latest News")
+        assertEquals(agency.channelCNN.news, "Breaking News")
     }
 }
